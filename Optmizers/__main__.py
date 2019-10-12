6b9a78d7a20f65d44b5efee12b9438525b57c6f6
elselim <elmekkiselim@gmail.com>
2019-10-12 13:16:22 +0200

    Removed older files, scripts and pdf and kivy (saved locally)

--- Optmizers/__main__.py
+++ Optmizers/__main__.py
@@ -5,6 +5,6 @@ from case2optimizer import Case2optimizer
 if __name__ == "__main__":
     with open("input_data.json") as json_file:
         data = json.loads(json_file.read())
-    Ofun = Case1optimizer(data_dict=data)
+    Ofun = Case2optimizer(data_dict=data)
     Ofun.solve_model()
     Ofun.show_results()
