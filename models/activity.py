3b2d68e07d54292ec103a937ca2f436660edfb4e
elselim <elmekkiselim@gmail.com>
2019-11-26 19:13:40 +0100

    Mean distance + check grouped list

--- models/activity.py
+++ models/activity.py
@@ -2,8 +2,8 @@ from models.destination import Destination
 from models.person import Person
 import importlib
 from math import sqrt
-
-
+from itertools import groupby
+import numpy as np
 class Activity:
     def __init__(self, data):
         self.people = [Person(p_data) for p_data in data["people"]]
@@ -15,11 +15,11 @@ class Activity:
         self.optimizer = class_(data["optimizer"])
         
     def compute_distance_dict(self):
-        def distance(A, B):
+        return {(i.name,j.name): self.distance(i.loc, j.loc) for i in self.people + self.destinations for j in self.people + self.destinations}
+
+    def distance(self, A, B):
             # Calculate the distance from A to B
             return sqrt((A[0]-B[0])**2 + (A[1]-B[1])**2)
-
-        return {(i.name,j.name): distance(i.loc, j.loc) for i in self.people + self.destinations for j in self.people + self.destinations}
 
     def compute_interval_score(self):
         while not all([d.flag_interval for d in self.destinations]):
@@ -34,24 +34,31 @@ class Activity:
 
             for d in self.destinations:
                 if not d.flag_interval:
-                    if any(d.intersection_list):
-                        d.flag_interval = True
-                    else:
+                    grouped_list = [(k, sum(1 for i in g)) for k,g in groupby(d.intersection_list)]
+                    print(d.intersection_list)
+                    for elem in grouped_list:
+                        if elem[0] == 1 and elem[1] >= self.duration:
+                            d.flag_interval = True
+                            break
+                    if not d.flag_interval:
                         d.interval_score -= 1
 
             for p in self.people:
                 p.increase_availabilities()
-            # print(self.people[0].increased_availabilities)
-            # for d in self.destinations:
-            #     print(d.name + " : " + str(d.interval_score))
+            #print(self.people[0].increased_availabilities)
+            for d in self.destinations:
+                print(d.name + " : " + str(d.interval_score))
 
 
                 
             
-
-        
-
     def run(self):
         self.optimizer._create_model(self.people, self.destinations, self.compute_distance_dict())
         self.optimizer.solve_model()
         self.optimizer.show_results(self.people, self.destinations)
+
+    
+    def compute_total_average_distance(self):
+        d_mean = np.mean([self.distance(i.loc, j.loc) for i in self.people for j in self.destinations])
+        print(d_mean)
+
