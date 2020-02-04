9d11808d46771e78e1e9f61d1d77ad327f5b896e
El Mekki Sélim <selim.elmekki@student.uliege.be>
2020-02-04 17:45:20 +0100

    changed request

--- make_request.py
+++ make_request.py
@@ -3,7 +3,11 @@ import json
 
 with open("input_data.json") as json_file:
     data = json.loads(json_file.read())
-response = requests.get('http://echkin.pythonanywhere.com/', json=data)
-#response = requests.get('http://localhost:5000/', json=data)
+
+response = requests.post('http://echkin.pythonanywhere.com/w3go/', json=data)
 print(response.content)
+response = requests.get('http://echkin.pythonanywhere.com/w3go/', json=data)
+print(response.content)
+
+#response = requests.get('http://localhost:5000/', json=data)
 # DOES NOT WORK LOCALLY -> USE NEOS (cplex)
